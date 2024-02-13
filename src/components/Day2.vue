cbc33108dd7345a5ec32fe776090ab69c5e32ba6
Orcoholist <orcoholist@mail.ru>
2024-02-13 21:49:25 +0300

    day-2

---
+++ src/components/Day2.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="app">
+      <h2>{{ greeting }}</h2>
+      <p>by {{ user }} who lives in {{ city }}</p>
+      <button v-on:click="changeGreeting">Change Greeting</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Hello World!",
+      user: "Aleksey Fedorov",
+      city: "Saint-Ptersburg",
+    };
+  },
+  methods: {
+    changeGreeting() {
+      this.greeting =
+        this.greeting === "Hello World!" ? "What is up!" : "Hello World!";
+    },
+  },
+};
+</script>
+<style>
+html,
+body {
+  background: #f3f5f6;
+}
+
+.app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+}
+
+h2 {
+  margin-top: 0;
+}
+
+p {
+  margin-top: 0;
+  font-size: 18px;
+}
+</style>

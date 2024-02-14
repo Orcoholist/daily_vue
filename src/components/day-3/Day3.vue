47d055b341a2706af29713d6240a4384a8021f8f
Orcoholist <orcoholist@mail.ru>
2024-02-14 22:17:16 +0300

    tojesamoe

---
+++ src/components/day-3/Day3.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="app">
+      <h2>{{ greeting }}</h2>
+      <p>by José Luis Ferrete who lives in Seville</p>
+      <button @click="changeGreeting">Change Greeting</button>
+    </div>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        greeting: "Hello World!"
+      };
+    },
+    methods: {
+      changeGreeting() {
+        this.greeting = this.greeting === "Hello World!" ? "What is up!" : "Hello World!";
+      }
+    }
+  };
+  </script>
+  <style>
+  </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Day3 from "../components/day-3/Day3.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/day2',
-      name: 'day2',
-      component: () => import('../components/Day2.vue')      
-    }
-  ]
-})
+      path: "/day2",
+      name: "day2",
+      component: () => import("../components/Day2.vue"),
+    },
+    {
+      path: "/day3",
+      name: "Day3",
+      component: () => import("../components/day-3/Day3.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
